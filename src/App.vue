b424b36d138424aae64c677d08e2d1f34f57f1a8
danielislas3 <danielislas3@live.com>
2020-12-29 17:02:04 -0600

    upload cloudinary component

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,36 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" :src="imageUrl" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <!-- <UploadCloudinary @imagenSubida="setUrl"></UploadCloudinary> -->
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import HelloWorld from "./components/HelloWorld.vue";
+// import UploadCloudinary from "@/components/UploadCloudinary";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+    // UploadCloudinary,
+  },
+  data() {
+    return {
+      url: "",
+    };
+  },
+  computed: {
+    imageUrl() {
+      return this.url === "" ? require("./assets/logo.png") : this.url;
+    },
+  },
+  methods: {
+    setUrl(e) {
+      this.url = e;
+    },
+  },
+};
 </script>
 
 <style>
---
+++ src/components/UploadCloudinary.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <div>
+      <button @click="openUploadModal">Upload files</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      url: "",
+    };
+  },
+
+  methods: {
+    openUploadModal() {
+      window.cloudinary
+        .openUploadWidget(
+          { cloud_name: "dcx0tg0qb", upload_preset: "xjcp86uk" },
+          (error, result) => {
+            if (!error && result && result.event === "success") {
+              console.log("Done uploading..: ", result.info);
+              this.$emit("imagenSubida", result.info.url);
+            }
+          }
+        )
+        .open();
+    },
+  },
+};
+</script>
